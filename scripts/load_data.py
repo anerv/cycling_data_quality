0e2b6b5dda4a5dbaa4a26edfed5d8d9f91e5ffb7
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-03-30 18:22:48 +0200

    added functions

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,63 +1,78 @@
 
 #%%
 import geopandas as gpd
+import pandas as pd
 import osmnx as ox
 import yaml
 import matplotlib.pyplot as plt
 import contextily as cx
 from datetime import datetime
 from src import graph_functions as gf
+from src import simplification_functions as sf
+from src import evaluation_functions as ef
 #%%
 with open(r'../config.yml') as file:
 
     parsed_yaml_file = yaml.load(file, Loader=yaml.FullLoader)
 
     study_area = parsed_yaml_file['study_area']
+    study_area_poly_fp = parsed_yaml_file['study_area_poly']
     study_crs = parsed_yaml_file['study_crs']
-    study_area_poly = parsed_yaml_file['study_area_poly']
+
+    use_custom_filter = parsed_yaml_file['use_custom_filter']
+    custom_filter = parsed_yaml_file['custom_filter']
+    standard_filter = parsed_yaml_file['standard_filter']
+    osm_way_tags = parsed_yaml_file['osm_way_tags']
 
     reference_comparison = parsed_yaml_file['reference_comparison']
     reference_fp = parsed_yaml_file['reference_fp']
     reference_geometries = parsed_yaml_file['reference_geometries']
+    column_bidirectional = parsed_yaml_file['column_bidirectional']
     
-    custom_filter = parsed_yaml_file['custom_filter']
-
+    
 print('Settings loaded!')
+
+ef.check_settings_validity(study_area, study_area_poly_fp, study_crs, use_custom_filter, custom_filter, reference_comparison,
+    reference_fp, reference_geometries, column_bidirectional)
+
 #%%
 ########## Study Area ##########
 
 # Read polygon for study area
-sa_poly = gpd.read_file(study_area_poly)
+study_area_poly = gpd.read_file(study_area_poly_fp)
 
-if sa_poly.crs == None:
+if study_area_poly.crs == None:
     print('Please assign a crs to the study area polygon!')
 
-if sa_poly.crs != study_crs:
-    sa_poly = sa_poly.to_crs(study_crs)
+if study_area_poly.crs != study_crs:
+    study_area_poly = study_area_poly.to_crs(study_crs)
 
-assert sa_poly.crs == study_crs
+assert study_area_poly.crs == study_crs
 
-area = sa_poly.area.values[0]
+area = study_area_poly.area.values[0]
 print(f'The size of the study area is {area / 1000000:.2f} square kilometers')
 
 fig, ax = plt.subplots(1, figsize=(15,15))
 
-sa_poly.plot(ax=ax, color='green', alpha=0.4)
+study_area_poly.plot(ax=ax, color='green', alpha=0.4)
 
 ax.set_axis_off()
 
 cx.add_basemap(
     ax, 
-    crs=sa_poly.crs, 
+    crs=study_area_poly.crs, 
     source=cx.providers.CartoDB.Voyager
 )
 
 #%%
 ########## PART 1: READ OSM DATA ##########
 
-# TODO: Method for getting graph based on custom filter
+# TODO: Method for getting graph based on custom filter?
 
-graph_osm = ox.graph_from_polygon(sa_poly.to_crs('EPSG:4326').loc[0, 'geometry'], network_type='bike', simplify=False, retain_all=True, truncate_by_edge=False, clean_periphery=True)
+ox.utils.config(use_cache=True, 
+    useful_tags_way = osm_way_tags)
+
+graph_osm = ox.graph_from_polygon(study_area_poly.to_crs('EPSG:4326').loc[0, 'geometry'], network_type='bike', simplify=False, retain_all=True, truncate_by_edge=False, clean_periphery=True)
 
 # Project graph to chosen crs
 graph_osm = ox.project_graph(graph_osm, to_crs=study_crs)
@@ -65,45 +80,92 @@ graph_osm = ox.project_graph(graph_osm, to_crs=study_crs)
 # Get osm_edges and osm_nodes
 osm_nodes, osm_edges = ox.graph_to_gdfs(graph_osm)
 
+# Clean up column names (removing ':' and upper case letters)
+osm_edges = gf.clean_col_names(osm_edges)
+osm_nodes = gf.clean_col_names(osm_nodes)
+
+# Add attribute on whether cycling infra exist or not (to be used by e.g. simplification function)
+osm_edges['cycling_infrastructure'] = 'no'
+
+queries = ["highway == 'cycleway'",
+        "cycleway in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway']",
+        "cycleway_left in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway']", # should shared_busway be included?
+        "cycleway_right in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway']",
+        "cycleway_both in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway']",
+        "bicycle_road == 'yes'"]
+
+for q in queries:
+    ox_filtered = osm_edges.query(q)
+
+    osm_edges.loc[ox_filtered.index, 'cycling_infrastructure'] = 'yes'
+
+osm_edges.cycling_infrastructure.value_counts()
+
 # Overview of data from OSM
-osm_na_poly = osm_nodes.unary_union.convex_hull # Use convex hull for area computation
-osm_poly_gdf = gpd.GeoDataFrame()
-osm_poly_gdf.at[0,'geometry'] = osm_na_poly
-osm_poly_gdf = osm_poly_gdf.set_crs(study_crs)
-
-graph_area = osm_poly_gdf.clip(sa_poly).area.values[0] # Clip in case convex hull goes beyond study area
-
+graph_area = ef.get_graph_area(osm_nodes, study_area_poly, study_crs)
 print(f'The graph covers an area of {graph_area/ 1000000:.2f} square kilometers')
 
-print(f'The length of the OSM network is {osm_edges.unary_union.length/1000 :.2f} kilometers')
+# Define whether cycling infrastructure is in both or one direction and whether it is one- or bidirectional
+osm_edges = ef.simplify_cycling_tags(osm_edges)
 
+# Measure the length of cycling infrastructure (!=from length of edges)
+osm_edges['infrastructure_length'] = osm_edges.apply(lambda x: ef.measure_infrastructure_length(edge = x.geometry, 
+                                                        geometry_type=x.cycling_geometries, bidirectional=x.cycling_bidirectional, cycling_infrastructure=x.cycling_infrastructure), axis=1)
 
+cycling_edges = osm_edges.loc[osm_edges.cycling_infrastructure == 'yes']
+print(f'The length of the OSM network is {cycling_edges.infrastructure_length.sum()/1000 :.2f} kilometers')
+
+#%%
+
+# TODO: Create a network out of only cycling infra - wrap in function
+
+#Getting a list of unique nodes used by bike_edges
+cycling_edges_index = pd.MultiIndex.to_frame(cycling_edges.index)
+u = cycling_edges_index['u'].to_list()
+v = cycling_edges_index['v'].to_list()
+
+used_nodes = list(set().union(u,v))
+
+#All nodes are copied to an new dataframe
+cycling_nodes = osm_nodes.copy(deep=True)
+
+#Creating new column in bike_nodes with the index value
+cycling_nodes['osmid'] = cycling_nodes.index
+
+#Using list of nodes to mask out unnecessary nodes
+cycling_nodes = cycling_nodes[cycling_nodes['osmid'].isin(used_nodes)]
+
+#Drop column - not needed anymore 
+cycling_nodes.drop(columns='osmid', inplace=True)
+
+#Create graph from nodes and edge geodataframe
+cycling_graph = ox.graph_from_gdfs(cycling_nodes, cycling_edges, graph_attrs=graph_osm.graph)
+
+#%%
 # Plot network
 fig, ax = plt.subplots(1, figsize=(15,15))
 
-osm_edges.plot(ax=ax, color='black', linewidth=0.2)
-osm_nodes.plot(ax=ax, color='black', markersize=0.2)
+cycling_edges.plot(ax=ax, color='black', linewidth=0.2)
+cycling_nodes.plot(ax=ax, color='black', markersize=0.2)
 
-sa_poly.plot(ax=ax, edgecolor='red', facecolor='None', linewidth=1)
+study_area_poly.plot(ax=ax, edgecolor='red', facecolor='None', linewidth=1)
 
 cx.add_basemap(
     ax=ax, 
-    crs=sa_poly.crs, 
+    crs=study_area_poly.crs, 
     source=cx.providers.CartoDB.Voyager
 )
 ax.set_axis_off()
-
-# TODO: Method for simplifying graph 
-graph_osm_simple = None
+#%%
+# TODO: Simplify without using info on cycling infrastructure type and 
+graph_osm_simple = sf.simplify_graph(graph_osm, attributes=['cycling_infrastructure','cycling_bidirectional','cycling_geometries'])
 #%%
 # Save data
-
 ox.save_graphml(graph_osm, f'../data/osm_{study_area}.graphml')
-
-#ox.save_graphml(graph_osm_simple, f'../data/osm_{study_area}_simple.graphml')
-
+ox.save_graphml(graph_osm_simple, f'../data/osm_{study_area}_simple.graphml')
 print('OSM networks saved!')
 
+# TODO: Save results of simplifying network
 # Save time for when OSM data was loaded
 current_time = datetime.now().strftime('%m/%d/%Y, %H:%M:%S')
 f = open('../osm_meta.txt', 'a')
@@ -128,24 +190,19 @@ if reference_comparison:
     assert ref_data.crs == study_crs
 
     # Clip reference data to study area poly
-    ref_data = ref_data.clip(sa_poly)
+    ref_data = ref_data.clip(study_area_poly)
 
     # Convert to osmnx graph object
-    graph_ref = gf.create_osmnx_graph(ref_data, directed=True)
+    graph_ref = gf.create_osmnx_graph(ref_data)
 
     ref_nodes, ref_edges = ox.graph_to_gdfs(graph_ref)
 
     # Overview of data
-    ref_na_poly = ref_nodes.unary_union.convex_hull # Use convex hull for area computation
-    ref_poly_gdf = gpd.GeoDataFrame()
-    ref_poly_gdf.at[0,'geometry'] = ref_na_poly
-    ref_poly_gdf = ref_poly_gdf.set_crs(study_crs)
-
-    graph_area = ref_poly_gdf.clip(sa_poly).area.values[0] # Clip in case convex hull goes beyond study area
-
+    graph_area = ef.get_graph_area(ref_nodes, study_area_poly, study_crs)
     print(f'The reference data covers an {graph_area / 1000000:.2f} square kilometers')
 
-    # TODO: Incorporate whether reference data is double or single geometries!
+
+    # TODO: Incorporate whether reference data is double or single geometries! Use function that creates a infrastructure length column?
     print(f'The length of the reference network is {ref_edges.unary_union.length/1000 :.2f} kilometers')
 
     # Plot network
@@ -154,19 +211,18 @@ if reference_comparison:
     ref_edges.plot(ax=ax, color='purple', linewidth=0.2)
     ref_nodes.plot(ax=ax, color='purple', markersize=0.2)
 
-    sa_poly.plot(ax=ax, edgecolor='red', facecolor='None', linewidth=1)
+    study_area_poly.plot(ax=ax, edgecolor='red', facecolor='None', linewidth=1)
 
     ax.set_axis_off()
 
     cx.add_basemap(
         ax, 
-        crs=sa_poly.crs, 
+        crs=study_area_poly.crs, 
         source=cx.providers.CartoDB.Voyager
     )
 
-    # Simplify
-    # TODO: Function for simplfying data
-    graph_ref_simple = None
+    # Simplify - the graph is simplified as much as possible, but without combining edges with conflicting values of cycling infrastructure types
+    graph_ref_simple = sf.momepy_simplify_graph(graph_ref, attributes=['vejklasse'])
 
     # Save data
     ox.save_graphml(graph_ref, f'../data/ref_{study_area}.graphml')
@@ -178,6 +234,3 @@ if reference_comparison:
 else:
     print('The analysis will not make use of a reference data set. Please update config settings if a extrinsic analysis of OSM data quality should be performed.')
 
-
-#%%
-########## PART 4: OSM METADATA ##########
---
+++ src/evaluation_functions.py
@@ -0,0 +1,126 @@
+import geopandas as gpd
+import os.path
+
+
+def check_settings_validity(study_area, study_area_poly_fp, study_crs, use_custom_filter, custom_filter, reference_comparison,
+    reference_fp, reference_geometries, column_bidirectional):
+    # Does not check for all potential errors, but givens an indication of whether settings have been filled out correctly
+
+    assert type(study_area) == str
+    assert os.path.exists(study_area_poly_fp) == True
+    assert type(study_crs) == str
+
+    if use_custom_filter == True:
+        assert custom_filter != None
+
+    if reference_comparison == True:
+        assert os.path.exists(reference_fp) == True
+
+    assert type(reference_geometries) == str
+    assert type(column_bidirectional) == str
+
+
+def get_graph_area(nodes, study_area_polygon, crs):
+
+    poly = nodes.unary_union.convex_hull # Use convex hull for area computation
+    poly_gdf = gpd.GeoDataFrame()
+    poly_gdf.at[0,'geometry'] = poly
+    poly_gdf = poly_gdf.set_crs(crs)
+
+    area = poly_gdf.clip(study_area_polygon).area.values[0] # Clip in case convex hull goes beyond study area
+
+    return area
+
+
+def simplify_cycling_tags(osm_edges):
+    # Does not take into account when there are differing types of cycling infrastructure in both sides?
+        
+    osm_edges['cycling_bidirectional'] = None
+    osm_edges['cycling_geometries'] = None
+
+    # Assumed to be one way if not explicitly stated that it is not
+    centerline_false_bidirectional_true = ["highway == 'cycleway' & (oneway=='no' or oneway_bicycle=='no')",
+                                "highway == 'bicycle_road' & (oneway =='no' or oneway_bicycle =='no')",
+                                "highway == 'track' & bicycle == 'designated' & (oneway=='no' or oneway_bicycle =='no')",
+                                "highway == 'path' & bicycle == 'designated' & (oneway=='no' or oneway_bicycle =='no')"]
+
+    centerline_false_bidirectional_false = ["highway == 'cycleway' & (oneway !='no' or oneway_bicycle != 'no')",
+                                "highway == 'bicycle_road' & (oneway!='no' or oneway_bicycle !='no')",
+                                "highway == 'track' & bicycle == 'designated' & (oneway !='no' or oneway_bicycle !='no')",
+                                "highway == 'path' & bicycle == 'designated' & (oneway !='no' or oneway_bicycle !='no')"]
+
+    # Only cycling infrastructure in one side, but it is explicitly tagged as not oneway
+    # or cycling infrastructure 
+    centerline_true_bidirectional_true = ["cycleway_left in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway','opposite'] and (cycleway_right in ['no','none','separate'] or cycleway_right.isnull()) and oneway_bicycle =='no'",
+                                "cycleway_right in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway','opposite'] and (cycleway_left in ['no','none','separate'] or cycleway_left.isnull()) and oneway_bicycle =='no'",
+                                "cycleway in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway','opposite'] and (oneway_bicycle == 'no' or oneway_bicycle.isnull())",
+                                "cycleway_both in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway','opposite'] and (oneway_bicycle == 'no' or oneway_bicycle.isnull())",
+                                "cycleway_left in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway','opposite'] and cycleway_right in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway']",
+                                "bicycle_road =='yes' and (oneway != 'yes' or oneway_bicycle !='yes')"]
+
+    # Only cycling infrastructure in one side and not bidirectional (if oneway_bicycle isn't explicitly yes, we assume that this type of tagging is one way)
+    centerline_true_bidirectional_false = ["cycleway_left in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway','opposite'] and (cycleway_right in ['no','none','separate'] or cycleway_right.isnull()) and oneway_bicycle !='no'",
+                                "cycleway_right in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway','opposite'] and (cycleway_left in ['no','none','separate'] or cycleway_left.isnull() ) and oneway_bicycle != 'no'",
+                                "cycleway in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway','opposite'] and oneway_bicycle == 'yes'",
+                                "cycleway_both in ['lane','track','opposite_lane','opposite_track','shared_lane','designated','crossing','share_busway','opposite'] and oneway_bicycle == 'yes'",
+                                "bicycle_road =='yes' and (oneway == 'yes' or oneway_bicycle =='yes')"]
+
+    # The order of the queries matter: To account for instances where highway=cycleway and cycleway=some value indicating cycling infrastructure, the queries classifying based on highway should be run lastest
+    for c in centerline_true_bidirectional_true:
+        ox_filtered = osm_edges.query(c)
+        osm_edges.loc[ox_filtered.index, 'cycling_bidirectional'] = True
+        osm_edges.loc[ox_filtered.index, 'cycling_geometries'] = 'centerline'
+    
+    for c in centerline_true_bidirectional_false:
+        ox_filtered = osm_edges.query(c, engine='python')
+        osm_edges.loc[ox_filtered.index, 'cycling_bidirectional'] = False
+        osm_edges.loc[ox_filtered.index, 'cycling_geometries'] = 'centerline'
+
+    
+    for c in centerline_false_bidirectional_true:
+        ox_filtered = osm_edges.query(c)
+        osm_edges.loc[ox_filtered.index, 'cycling_bidirectional'] = True
+        osm_edges.loc[ox_filtered.index, 'cycling_geometries'] = 'true_geometries'
+
+    for c in centerline_false_bidirectional_false:
+        ox_filtered = osm_edges.query(c)
+        osm_edges.loc[ox_filtered.index, 'cycling_bidirectional'] = False
+        osm_edges.loc[ox_filtered.index, 'cycling_geometries'] = 'true_geometries'
+
+
+    # Assert that cycling bidirectional and cycling geometries have been filled out for all where cycling infrastructure is yes!
+    assert len(osm_edges.query("cycling_infrastructure =='yes' & (cycling_bidirectional.isnull() or cycling_geometries.isnull())")) == 0, 'Not all cycling infrastructure has been classified!'
+
+    print(osm_edges.cycling_bidirectional.value_counts())
+    print(osm_edges.cycling_geometries.value_counts())
+
+    return osm_edges
+
+
+def measure_infrastructure_length(edge, geometry_type, bidirectional, cycling_infrastructure):
+
+    edge_length = edge.length
+
+    # TODO: Simplify - only need cycling infrastructure and bidirectional?
+
+    if cycling_infrastructure == 'yes' and geometry_type == 'true_geometries' and bidirectional == True:
+        infrastructure_length = edge_length * 2
+      
+    elif cycling_infrastructure == 'yes' and geometry_type == 'true_geometries' and bidirectional == False:
+        infrastructure_length = edge_length
+     
+    elif cycling_infrastructure == 'yes' and geometry_type == 'centerline' and bidirectional == True:
+        infrastructure_length = edge_length * 2
+       
+    elif cycling_infrastructure == 'yes' and geometry_type == 'centerline' and bidirectional == False:
+        infrastructure_length = edge_length
+        
+ 
+    elif cycling_infrastructure == 'yes' and (geometry_type is None or bidirectional is None):
+        print('Missing information when calculating true infrastructure length!')
+        infrastructure_length = None
+    
+    else:
+        infrastructure_length = None
+
+    return infrastructure_length
