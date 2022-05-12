8f7e16d3b39e149acd6de4720dba5d8a141b2fb6
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-05-12 10:55:07 +0200

    upd func

--- src/matching_functions.py
+++ src/matching_functions.py
@@ -582,6 +582,76 @@ def find_parallel_edges(edges):
 
 ##############################
 
+# def create_osmnx_graph_mf(gdf):
+
+#     ''''
+#     Function for  converting a geodataframe with LineStrings to a NetworkX graph object (MultiDiGraph), which follows the data structure required by OSMnx.
+#     (I.e. Nodes indexed by osmid, nodes contain columns with x and y coordinates, edges is multiindexed by u, v, key).
+#     Converts MultiLineStrings to LineStrings - assumes that there are no gaps between the lines in the MultiLineString
+
+#     OBS! Current version does not fix topology.
+
+#     Parameters
+#     ----------
+#     gdf: GeoDataFrame
+#         The data to be converted to a graph format
+#     directed: bool
+#         Whether the resulting graph should be directed or not. Directionality is based on the order of the coordinates.
+
+#     Returns
+#     -------
+#     graph: NetworkX MultiDiGraph object
+#         The original data in a NetworkX graph format.
+
+#     '''
+
+#     gdf['geometry'] = gdf['geometry'].apply( lambda x: linemerge(x) if x.geom_type == 'MultiLineString' else x)
+
+#     # If Multilines cannot be merged do to gaps, use explode
+#     geom_types = gdf.geom_type.to_list()
+#     #unique_geom_types = set(geom_types)
+
+#     if 'MultiLineString' in geom_types:
+#         gdf = gdf.explode(index_parts=False)
+
+#     # TODO: Convert linestrings to edges?
+#     G = momepy.gdf_to_nx(gdf, approach='primal', directed=True)
+
+#     nodes, edges = momepy.nx_to_gdf(G)
+
+#     # Create columns and index as required by OSMnx
+#     index_length = len(str(nodes['nodeID'].iloc[-1].item()))
+#     nodes['osmid'] = nodes['nodeID'].apply(lambda x: create_node_index(x, index_length, add_letter=False))
+
+#     # Create x y coordinate columns
+#     nodes['x'] = nodes.geometry.x
+#     nodes['y'] = nodes.geometry.y
+
+#     edges['u'] = nodes['osmid'].loc[edges.node_start].values
+#     edges['v'] = nodes['osmid'].loc[edges.node_end].values
+
+#     nodes.set_index('osmid', inplace=True)
+
+#     edges['length'] = edges.geometry.length # Length is required by some functions
+
+#     edges['key'] = 0
+
+#     edges = find_parallel_edges(edges)
+
+#     # Create multiindex in u v key format
+#     edges = edges.set_index(['u', 'v', 'key'])
+
+#     # For ox simplification to work, edge geometries must be dropped. Edge geometries is defined by their start and end node
+#     #edges.drop(['geometry'], axis=1, inplace=True) # Not required by new simplification function
+
+
+#     G_ox = ox.graph_from_gdfs(nodes, edges)
+
+   
+#     return G_ox
+
+##############################
+
 def create_osmnx_graph(gdf):
 
     ''''
