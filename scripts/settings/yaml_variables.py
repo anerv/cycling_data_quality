f5ac24d7baad826fb2d459de134d20b63dd38638
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-09-26 15:34:04 +0200

    renamed dict

--- scripts/settings/yaml_variables.py
+++ scripts/settings/yaml_variables.py
@@ -25,7 +25,7 @@ with open(r'../../config.yml') as file:
 
     grid_cell_size = parsed_yaml_file['grid_cell_size']
 
-    missing_tag_dict = parsed_yaml_file['missing_tag_analysis']
+    existing_tag_dict = parsed_yaml_file['existing_tag_analysis']
     incompatible_tags_dict = parsed_yaml_file['incompatible_tags_analysis']
 
 study_area_poly_fp = f'../../data/study_area_polygon/{study_area}/study_area_polygon.gpkg'
