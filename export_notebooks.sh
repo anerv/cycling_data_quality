fefe31f28b785b35d38afd12409d50e7288662e8
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-10-13 16:27:16 +0200

    added sh file for exporting notebooks

---
+++ export_notebooks.sh
@@ -0,0 +1,16 @@
+# Export all notebooks as HTML without code cells included
+
+# Load data notebooks
+jupyter nbconvert scripts/osm/01a_load_osm.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
+
+jupyter nbconvert scripts/reference/01b_load_reference.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
+
+# Intrinsic notebooks
+jupyter nbconvert scripts/osm/02a_intrinsic_analysis_OSM.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
+
+jupyter nbconvert scripts/reference/02b_intrinsic_analysis_ref.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
+
+# Extrinsic notebooks
+jupyter nbconvert scripts/compare/03a_extrinsic_analysis_metrics.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
+
+jupyter nbconvert scripts/compare/03b_extrinsic_analysis_feature_matching.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
