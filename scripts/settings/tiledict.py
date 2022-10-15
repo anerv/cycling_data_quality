48d52e659f469eafc5215eac38860d1d87d5a6a3
Anastassia Vybornova <anastasiavybornova@gmail.com>
2022-10-15 10:25:41 +0200

    updates for 01a, 01b, 02a

--- scripts/settings/tiledict.py
+++ scripts/settings/tiledict.py
@@ -1,6 +1,14 @@
 import folium 
 
 folium_layers = {
+        'Stamen TonerLite': folium.TileLayer(
+                tiles = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png',
+                attr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                name = 'Stamen TonerLite',
+                control = True,
+                overlay = True,
+                show = True
+        ), 
         'Google Satellite': folium.TileLayer(
                 tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
                 attr = 'Google',
@@ -9,22 +17,6 @@ folium_layers = {
                 control = True,
                 show = False
                 ),
-        'whiteback': folium.TileLayer(
-                tiles = 'https://api.mapbox.com/styles/v1/krktalilu/ckrdjkf0r2jt217qyoai4ndws/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia3JrdGFsaWx1IiwiYSI6ImNrcmRqMXdycTB3NG8yb3BlcGpiM2JkczUifQ.gEfOn5ttzfH5BQTjqXMs3w',
-                name = 'Background: White',
-                attr = 'Mapbox',
-                control = True,
-                overlay = True,
-                show = False
-                ),
-        'Stamen TonerLite': folium.TileLayer(
-                tiles = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png',
-                attr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                name = 'Stamen TonerLite',
-                control = True,
-                overlay = True,
-                show = False
-        ), 
         'CyclOSM': folium.TileLayer(
                 tiles = 'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
                 attr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -38,6 +30,7 @@ folium_layers = {
                 name = 'OpenStreetMap',
                 attr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
                 control = True, 
-                overlay = True
+                overlay = True,
+                show = False
                 )
 }
