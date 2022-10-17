f118c790d16cc1f1ca2088f68a148c6010af3b1b
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-10-17 15:58:22 +0200

    added no_export tag

--- export_notebooks.sh
+++ export_notebooks.sh
@@ -6,11 +6,11 @@ jupyter nbconvert scripts/osm/01a_load_osm.ipynb --TagRemovePreprocessor.enabled
 jupyter nbconvert scripts/reference/01b_load_reference.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
 
 # Intrinsic notebooks
-jupyter nbconvert scripts/osm/02a_intrinsic_analysis_OSM.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
+jupyter nbconvert scripts/osm/02a_intrinsic_analysis_OSM.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags no_export --no-input --to html
 
-jupyter nbconvert scripts/reference/02b_intrinsic_analysis_ref.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
+jupyter nbconvert scripts/reference/02b_intrinsic_analysis_ref.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags no_export --no-input --to html
 
 # Extrinsic notebooks
-jupyter nbconvert scripts/compare/03a_extrinsic_analysis_metrics.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
+jupyter nbconvert scripts/compare/03a_extrinsic_analysis_metrics.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags no_export --no-input --to html
 
-jupyter nbconvert scripts/compare/03b_extrinsic_analysis_feature_matching.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove_cell --no-input --to html
+jupyter nbconvert scripts/compare/03b_extrinsic_analysis_feature_matching.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags no_export --no-input --to html
