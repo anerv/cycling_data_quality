04ec793ac8a959f7578596a13e2b896663565c2c
Michael Szell <michael.szell@gmail.com>
2022-12-12 22:25:08 +0100

    added analysistype to titlepage

--- templates/postprocess_html.sh
+++ templates/postprocess_html.sh
@@ -20,6 +20,15 @@ study_area=${study_area##study_area: }
 cp templates/titlepage_template.html exports/"$study_area"/html/titlepage.html
 sed -i "" -e "s/\[study_area_humanreadable\]/${study_area_humanreadable}/g" exports/"$study_area"/html/titlepage.html
 sed -i "" -e "s/\[timestamp\]/$(date "+%Y-%m-%d %H:%M:%S")/g" exports/"$study_area"/html/titlepage.html
+if [ $mode == 1 ] || [ $mode == 2 ];
+then
+	analysistype="Intrinsic"
+else
+	analysistype="Intrinsic \&amp; Extrinsic"
+fi
+sed -i "" -e "s/\[analysistype\]/$analysistype/g" exports/"$study_area"/html/titlepage.html
+
+
 if [ $mode == 1 ];
 then
 	cp results/OSM/"$study_area"/maps_static/titleimage.png exports/"$study_area"/html/titleimage.png
