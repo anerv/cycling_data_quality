2145adc536f010a8e0c6fcd39e4cddab3f6e6eac
Ane Rahbek Vierø <vieroe@hotmail.com>
2022-12-22 09:58:34 +0100

    updated load data scripts

--- scripts/settings/load_osmdata.py
+++ scripts/settings/load_osmdata.py
@@ -1,8 +1,7 @@
-# This data is prepared in the 01a_initialize_osm notebook
+# This data is prepared in the 1a_initialize_osm notebook
 
 import osmnx as ox
 import geopandas as gpd
-import pickle
 
 exec(open("../settings/yaml_variables.py").read())
 exec(open("../settings/paths.py").read())
@@ -27,29 +26,25 @@ osm_grid = gpd.read_file(osm_grid_fp)
 grid_ids = osm_grid.grid_id.to_list()
 
 # Load saved edged and nodes
-with open(osm_nodes_fp, "rb") as fp:
-    osm_nodes = pickle.load(fp)
+osm_nodes = gpd.read_file(osm_nodes_fp)
+osm_nodes.set_index("osmid", inplace=True)
 
-with open(osm_edges_fp, "rb") as fp:
-    osm_edges = pickle.load(fp)
+osm_edges = gpd.read_file(osm_edges_fp)
+osm_edges.set_index(["u", "v", "key"], inplace=True)
 
-with open(osm_nodes_simplified_fp, "rb") as fp:
-    osm_nodes_simplified = pickle.load(fp)
+osm_edges_simplified = gpd.read_file(osm_edges_simplified_fp)
+osm_edges_simplified.set_index(["u", "v", "key"], inplace=True)
 
-with open(osm_edges_simplified_fp, "rb") as fp:
-    osm_edges_simplified = pickle.load(fp)
+osm_nodes_simplified = gpd.read_file(osm_nodes_simplified_fp)
+osm_nodes_simplified.set_index("osmid", inplace=True)
+osm_nodes_simplified["osmid"] = osm_nodes_simplified.index
 
-# Joined data
-with open(osm_nodes_joined_fp, "rb") as fp:
-    osm_nodes_joined = pickle.load(fp)
+osm_edges_joined = gpd.read_file(osm_edges_joined_fp)
 
-with open(osm_edges_joined_fp, "rb") as fp:
-    osm_edges_joined = pickle.load(fp)
+osm_nodes_joined = gpd.read_file(osm_nodes_joined_fp)
 
-with open(osm_nodes_simplified_joined_fp, "rb") as fp:
-    osm_nodes_simp_joined = pickle.load(fp)
+osm_edges_simp_joined = gpd.read_file(osm_edges_simplified_joined_fp)
 
-with open(osm_edges_simplified_joined_fp, "rb") as fp:
-    osm_edges_simp_joined = pickle.load(fp)
+osm_nodes_simp_joined = gpd.read_file(osm_nodes_simplified_joined_fp)
 
 print("OSM data loaded successfully!")
